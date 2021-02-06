2449f4f2c37df18c2d280d210a422ed808fa87d1
Christoph Stade <christoph-stade@outlook.de>
2021-02-06 15:25:19 +0100

    formatting

--- day02.py
+++ day02.py
@@ -13,15 +13,13 @@ def cutString(line: list):
 for i in lines:
     currLine = cutString(i)
 
-     # part one 
-
+    # part one 
     counter = 0
     for pos in currLine[3]:
         counter += 1 if pos == currLine[2] else 0
     valids += 1 if counter >= int(currLine[0]) and counter <= int(currLine[1]) else 0
 
     # part two
-
     a = currLine[3][int(currLine[0]) - 1] == currLine[2] 
     b = currLine[3][int(currLine[1]) - 1] == currLine[2]
     valids2 += 1 if a != b else 0 # XOR
